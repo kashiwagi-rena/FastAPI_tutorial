4c574c9f8de08bbc8e80af5a3d677a72a68a35b5
kashiwagi-rena <azisai07@gmail.com>
2023-12-29 23:15:05 +0900

    パラメータとして宣言

--- app/main.py
+++ app/main.py
@@ -1,11 +1,18 @@
 from enum import Enum
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
+
+class item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
 
 app = FastAPI()
 
@@ -14,6 +21,10 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/items/2/")
+async def create_item(item: Item):
+    return item
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
