8e065e81d121b6b678c83ccd262f11f296c34c26
kashiwagi-rena <azisai07@gmail.com>
2023-12-29 22:51:58 +0900

    必須のクエリパラメータ

--- app/main.py
+++ app/main.py
@@ -31,6 +31,13 @@ async def read_user_item(item_id: str, needy: str):
     item = {"item_id": item_id, "needy": needy}
     return item
 
+@app.get("/item/3/{item_id}")
+async def read_user_item(
+    item_id: str, needy: str, skip: int = 0, limit: Union[int, None] = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
+
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
