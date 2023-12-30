708c2cdd659b9d0862d0a0a751f3105734f1e453
kashiwagi-rena <azisai07@gmail.com>
2023-12-30 18:06:59 +0900

    デフォルト値

--- app/main.py
+++ app/main.py
@@ -59,6 +59,13 @@ async def read_items(
         results.update({"q": q})
     return results
 
+@app.get("/items/6")
+async def read_items(q: str = Query(default="fixedquery", min_length=3)):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
 @app.get("/items/5")
 async def read_items(q: Union[str, None] = Query(default=None, min_length=3, max_length=50)):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
