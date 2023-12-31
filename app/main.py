7ab64bd1cb4af9b3ee250d1f1bcb0e2740a7938b
kashiwagi-rena <azisai07@gmail.com>
2023-12-31 16:06:19 +0900

    Annotatedを使って必要なパラメータを並べる

--- app/main.py
+++ app/main.py
@@ -126,7 +126,9 @@ async def create_item(item_id: int, item: Item, q: Union[str, None] = None):
     return result
 
 @app.get("/items/6/{item_id}")
-async def read_items(q: str, item_id: int = Path(title="The ID of the to get")):
+async def read_items(
+    q: str, item_id: Annotated[int, Path(title="The ID of the item to get")]
+):
     results = {"item_id": item_id}
     if q:
         results.update({"q": q})
