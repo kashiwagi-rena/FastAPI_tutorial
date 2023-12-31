23491805c6ccd8d01afff9b00e0393251f2d7379
kashiwagi-rena <azisai07@gmail.com>
2023-12-31 16:34:38 +0900

    数値の検証：以上または等しい

--- app/main.py
+++ app/main.py
@@ -127,7 +127,7 @@ async def create_item(item_id: int, item: Item, q: Union[str, None] = None):
 
 @app.get("/items/6/{item_id}")
 async def read_items(
-    item_id: Annotated[int, Path(title="The ID of the item to get")], q: str
+    item_id: Annotated[int, Path(title="The ID of the item to get", ge=1)], q: str
 ):
     results = {"item_id": item_id}
     if q:
